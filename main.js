310d4c15240d74e07383943048694c89b64845e9
jungjunhyeok <ted0505@naver.com>
2023-02-21 01:04:34 +0900

    Design: Change

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const toggleBtn = document.querySelector('.navbar__toogleBtn');
 const menu = document.querySelector('.navbar__menu');
+const canvas = document.querySelector('#webgl-canvas');
+const main_theme = document.querySelector('.main_theme');
 // const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
-    // icons.classList.toggle('active');
+    canvas.classList.toggle('active');
+    main_theme.classList.toggle('active');
 }) 
