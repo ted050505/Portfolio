af4dbe287b20a8aafda37a35c16eb9913c1997e6
jungjunhyeok <ted0505@naver.com>
2023-02-21 12:20:12 +0900

    Design: Add filter blur

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const toggleBtn = document.querySelector('.navbar__toogleBtn');
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const toggleBtn_cancel = document.querySelector('.navbar__toggleBtn__cancel');
 const menu = document.querySelector('.navbar__menu');
 const canvas = document.querySelector('#webgl-canvas');
 const main_theme = document.querySelector('.main_theme');
-// const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     canvas.classList.toggle('active');
     main_theme.classList.toggle('active');
+    // toggleBtn_cancel.classList.toggle('active');
+    // toggleBtn.classList.toggle('active');
 }) 
+
+// toggleBtn_cancel.addEventListener('click', () => {
+//     toggleBtn.classList.toggle('active');
+//     toggleBtn_cancel.classList.toggle('active');
+// })
