1f3b903b4f37346b5bf95593db5910fd00611532
jungjunhyeok <ted0505@naver.com>
2023-02-22 15:18:32 +0900

    Design: Change  웹 폰트 파일확장자에 따른 순서 변경 (최적화)

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const toggleBtn_cancel = document.querySelector('.navbar__toggleBtn__cancel');
 const menu = document.querySelector('.navbar__menu');
 const canvas = document.querySelector('#webgl-canvas');
+const wrapper = document.querySelector('#wrapper');
 const main_theme = document.querySelector('.main_theme');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     canvas.classList.toggle('active');
     main_theme.classList.toggle('active');
+    wrapper.classList.toggle('active');
     // toggleBtn_cancel.classList.toggle('active');
     // toggleBtn.classList.toggle('active');
 }) 
